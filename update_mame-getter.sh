13abd170cc8d5c5f413cc8fd79779e5565ac303a
MAME-GETTER <64264825+MAME-GETTER@users.noreply.github.com>
2020-04-24 14:33:10 -0400

    Update update_mame-getter.sh

--- update_mame-getter.sh
+++ update_mame-getter.sh
@@ -2,11 +2,12 @@
 #set -x
 
 #Q: How does it work?
-#A: Run it like any other MiSTer update script. This script looks for .mra files in _Arcade and its recursive directories and tries to download the MAME merged set zip files listed in the .mra files. At the bottom of the script is a list of all the merged MAME roms for the .220 set. Only rom files with names that match this list will be downloaded.  
-# 
-#Q: Why should you use this script? 
-#A: Well you really shouldn't. It's for being bleeding edge when retrodriven has yet to update their files. I use this script download the mame zip after a new core/mra has been released.
+#A: Download it to the Scripts Directory and run it like any other MiSTer update script. This script looks for .mra files in _Arcade and its recursive directories and tries to download the MAME merged set zip files listed in the .mra files. At the bottom of the script is a list of all the merged MAME roms for the .220 set. Only rom files with names that match this list will be downloaded.  
 #
+#Q:Can I set my own custom loactions for MRA and MAME files? 
+#A:A /media/fat/Scripts/update_mame-getter.ini file may be used to set custom location for your MAME files and MRA files.
+#Add the following line to the ini file to set a directory for MRA files: MRADIR=/top/path/to/mra/files
+#Add the following line to the ini file to set a directory for MAME files: ROMDIR=/path/to/mame
 #
 #Q:Will this script over write files I already have?
 #A:NO, This script will not clober files you already have. You need to manaully remove any files you have if you want to download new files.
